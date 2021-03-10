f9b9ed6659290f96d5db7be5037ebdedad16f62f
Sergey <s.koldaev@haulmont.com>
2021-03-10 15:52:00 +0300

    fix problems with cluster(jar) and singleJar. Check work cluster(war). TODO: singleWar deploy

--- runappWithoutStarting.sh
+++ runappWithoutStarting.sh
@@ -1,6 +1,13 @@
+#gradle buildUberJar
+#gradle moveJars
+#cd docker-image
+#docker build -t javamelody ./javamelody #build image for javaMelody WAR-collect server
+#docker build -t cuba-sample-sales-core ./core
+#docker build -t cuba-sample-sales-web ./web
+
+
 gradle buildUberJar
 gradle moveJars
 cd docker-image
 docker build -t javamelody ./javamelody #build image for javaMelody WAR-collect server
-docker build -t cuba-sample-sales-core ./core
-docker build -t cuba-sample-sales-web ./web
+docker build -t cuba-sample-sales-singlejar ./singlejar
